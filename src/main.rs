3d448653e55c90124a5e46cb249b8dba2fe41374
Norgil <norgil.spb@gmail.com>
2023-01-23 18:13:52 +0200

    cat alternative with 256kb buffer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io::{stdin, stdout, Read, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mut buf = [0u8; 262144];
+    loop {
+        let n = stdin().read(&mut buf).expect("read failed");
+        if n == 0 {
+            break;
+        }
+        stdout().write_all(&buf[..n]).expect("write failed");
+    }
 }
